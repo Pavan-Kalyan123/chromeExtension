87fee9e81c8e8af896ef2941960ce070e4e7dd8a
Pavan-Kalyan123 <163886633+Pavan-Kalyan123@users.noreply.github.com>
2024-10-23 12:38:33 +0000

    Add files via upload

---
+++ appOn.js
@@ -0,0 +1,11 @@
+//appOn.js
+//alert("its working");
+(function (){
+document.querySelector("html").style.filter = "invert(1) hue-rotate(180deg)";
+
+let media = document.querySelectorAll("img, picture,video");
+
+media.forEach((mediaItem)=>{
+    mediaItem.style.filter =  "invert(1) hue-rotate(180deg)";
+})
+})();
